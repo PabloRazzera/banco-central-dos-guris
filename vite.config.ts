0dbd470d9ca07b075c8e4e50ee35d75e6745cd3c
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-09-11 20:23:50 -0300

    Configura GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/banco-central-dos-guris/',
   plugins: [react(), tailwindcss()],
 })
